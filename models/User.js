443aa0d3ed7871c80c84ca55dc4d07d21286fcbc
mickirobles-cr <juanmi.robles21@gmail.com>
2025-12-08 00:39:02 -0600

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,10 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    createdAt: { type: Date, default: Date.now }
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user"
+  }
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", UserSchema);
